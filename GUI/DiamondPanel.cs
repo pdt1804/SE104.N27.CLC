b037764fb12f433ff57538704d54d8a21e135cf7
eugeous <121101254+Eugeou@users.noreply.github.com>
2023-06-07 17:31:47 +0700

    Viết method để vẽ hình thoi trong panel

--- GUI/DiamondPanel.cs
+++ GUI/DiamondPanel.cs
@@ -26,6 +26,39 @@ namespace GUI
             GradientAngle = 45;
             BorderRadius = 10;
         }
+        // Methods
+        private GraphicsPath GetDiamondPath(RectangleF rectangle, float borderRadius)
+        {
+            GraphicsPath path = new GraphicsPath();
 
-    }  
+            float radius = borderRadius / 2;
+            PointF top = new PointF(rectangle.Left + rectangle.Width / 2, rectangle.Top);
+            PointF right = new PointF(rectangle.Right, rectangle.Top + rectangle.Height / 2);
+            PointF bottom = new PointF(rectangle.Left + rectangle.Width / 2, rectangle.Bottom);
+            PointF left = new PointF(rectangle.Left, rectangle.Top + rectangle.Height / 2);
+
+            path.AddArc(top.X - radius, top.Y, borderRadius, borderRadius, 180, 90);
+            path.AddArc(right.X - borderRadius, right.Y - radius, borderRadius, borderRadius, 270, 90);
+            path.AddArc(bottom.X - radius, bottom.Y - borderRadius, borderRadius, borderRadius, 0, 90);
+            path.AddArc(left.X, left.Y - radius, borderRadius, borderRadius, 90, 90);
+
+            path.CloseFigure();
+
+            return path;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            Graphics g = e.Graphics;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+
+            using (Brush brush = new LinearGradientBrush(ClientRectangle, GradientTopColor, GradientBottomColor, GradientAngle))
+            {
+                GraphicsPath diamondPath = GetDiamondPath(ClientRectangle, BorderRadius);
+                g.FillPath(brush, diamondPath);
+            }
+        }
+    }
 }

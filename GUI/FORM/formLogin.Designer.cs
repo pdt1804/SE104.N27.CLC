4460077ed5df3cd965822694f7b7903290da292a
eugeous <121101254+Eugeou@users.noreply.github.com>
2023-06-08 01:13:47 +0700

    Sửa UI fornLogin

--- GUI/FORM/formLogin.Designer.cs
+++ GUI/FORM/formLogin.Designer.cs
@@ -35,13 +35,17 @@
             this.exit = new Siticone.Desktop.UI.WinForms.SiticoneControlBox();
             this.siticoneControlBox2 = new Siticone.Desktop.UI.WinForms.SiticoneControlBox();
             this.siticoneCustomGradientPanel1 = new Siticone.Desktop.UI.WinForms.SiticoneCustomGradientPanel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtUsername = new Siticone.Desktop.UI.WinForms.SiticoneTextBox();
-            this.txtUserpwd = new Siticone.Desktop.UI.WinForms.SiticoneTextBox();
             this.butLogin = new Siticone.Desktop.UI.WinForms.SiticoneGradientButton();
+            this.txtUserpwd = new Siticone.Desktop.UI.WinForms.SiticoneTextBox();
+            this.txtUsername = new Siticone.Desktop.UI.WinForms.SiticoneTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.siticonePictureBox1 = new Siticone.Desktop.UI.WinForms.SiticonePictureBox();
             this.customPictureBox1 = new GUI.CustomPictureBox();
+            this.customPanel1 = new GUI.CustomPanel();
             this.siticoneCustomGradientPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.siticonePictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.customPictureBox1)).BeginInit();
+            this.customPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // siticoneBorderlessForm1
@@ -66,9 +70,9 @@
             // 
             this.exit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.exit.FillColor = System.Drawing.Color.Transparent;
-            this.exit.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.exit.ForeColor = System.Drawing.Color.Black;
             this.exit.IconColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.exit.Location = new System.Drawing.Point(1177, -3);
+            this.exit.Location = new System.Drawing.Point(1038, 2);
             this.exit.Name = "exit";
             this.exit.Size = new System.Drawing.Size(59, 60);
             this.exit.TabIndex = 2;
@@ -77,11 +81,12 @@
             // siticoneControlBox2
             // 
             this.siticoneControlBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.siticoneControlBox2.BackColor = System.Drawing.Color.Transparent;
             this.siticoneControlBox2.ControlBoxType = Siticone.Desktop.UI.WinForms.Enums.ControlBoxType.MinimizeBox;
             this.siticoneControlBox2.FillColor = System.Drawing.Color.Transparent;
-            this.siticoneControlBox2.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.siticoneControlBox2.ForeColor = System.Drawing.Color.Black;
             this.siticoneControlBox2.IconColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.siticoneControlBox2.Location = new System.Drawing.Point(1102, -3);
+            this.siticoneControlBox2.Location = new System.Drawing.Point(963, 3);
             this.siticoneControlBox2.Name = "siticoneControlBox2";
             this.siticoneControlBox2.Size = new System.Drawing.Size(57, 60);
             this.siticoneControlBox2.TabIndex = 1;
@@ -90,64 +95,45 @@
             // siticoneCustomGradientPanel1
             // 
             this.siticoneCustomGradientPanel1.BackColor = System.Drawing.Color.Transparent;
-            this.siticoneCustomGradientPanel1.BorderRadius = 50;
+            this.siticoneCustomGradientPanel1.BorderColor = System.Drawing.Color.Transparent;
+            this.siticoneCustomGradientPanel1.BorderRadius = 90;
             this.siticoneCustomGradientPanel1.Controls.Add(this.butLogin);
             this.siticoneCustomGradientPanel1.Controls.Add(this.txtUserpwd);
             this.siticoneCustomGradientPanel1.Controls.Add(this.txtUsername);
             this.siticoneCustomGradientPanel1.Controls.Add(this.label1);
             this.siticoneCustomGradientPanel1.FillColor = System.Drawing.Color.CornflowerBlue;
             this.siticoneCustomGradientPanel1.FillColor2 = System.Drawing.Color.BlueViolet;
-            this.siticoneCustomGradientPanel1.FillColor3 = System.Drawing.Color.RoyalBlue;
+            this.siticoneCustomGradientPanel1.FillColor3 = System.Drawing.Color.DodgerBlue;
             this.siticoneCustomGradientPanel1.FillColor4 = System.Drawing.Color.BlueViolet;
-            this.siticoneCustomGradientPanel1.Location = new System.Drawing.Point(687, 84);
+            this.siticoneCustomGradientPanel1.Location = new System.Drawing.Point(-11, 0);
             this.siticoneCustomGradientPanel1.Name = "siticoneCustomGradientPanel1";
-            this.siticoneCustomGradientPanel1.Size = new System.Drawing.Size(498, 504);
+            this.siticoneCustomGradientPanel1.Size = new System.Drawing.Size(496, 446);
             this.siticoneCustomGradientPanel1.TabIndex = 10;
             // 
-            // label1
+            // butLogin
             // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(124, 35);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(247, 59);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Đăng Nhập";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // txtUsername
-            // 
-            this.txtUsername.BackColor = System.Drawing.Color.Transparent;
-            this.txtUsername.BorderColor = System.Drawing.Color.Black;
-            this.txtUsername.BorderRadius = 15;
-            this.txtUsername.BorderThickness = 2;
-            this.txtUsername.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.txtUsername.DefaultText = "";
-            this.txtUsername.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
-            this.txtUsername.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
-            this.txtUsername.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
-            this.txtUsername.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
-            this.txtUsername.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(196)))), ((int)(((byte)(255)))));
-            this.txtUsername.FocusedState.ForeColor = System.Drawing.Color.SlateBlue;
-            this.txtUsername.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.txtUsername.ForeColor = System.Drawing.Color.MediumPurple;
-            this.txtUsername.HoverState.BorderColor = System.Drawing.Color.Silver;
-            this.txtUsername.Location = new System.Drawing.Point(57, 161);
-            this.txtUsername.Name = "txtUsername";
-            this.txtUsername.PasswordChar = '\0';
-            this.txtUsername.PlaceholderForeColor = System.Drawing.Color.LightSteelBlue;
-            this.txtUsername.PlaceholderText = "username";
-            this.txtUsername.SelectedText = "";
-            this.txtUsername.Size = new System.Drawing.Size(361, 50);
-            this.txtUsername.TabIndex = 0;
-            this.txtUsername.TextChanged += new System.EventHandler(this.txtUsername_TextChanged);
+            this.butLogin.BackColor = System.Drawing.Color.Transparent;
+            this.butLogin.BorderColor = System.Drawing.Color.Pink;
+            this.butLogin.BorderRadius = 15;
+            this.butLogin.BorderThickness = 3;
+            this.butLogin.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.butLogin.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.butLogin.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.butLogin.DisabledState.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.butLogin.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.butLogin.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butLogin.ForeColor = System.Drawing.SystemColors.Window;
+            this.butLogin.Location = new System.Drawing.Point(120, 335);
+            this.butLogin.Name = "butLogin";
+            this.butLogin.Size = new System.Drawing.Size(240, 51);
+            this.butLogin.TabIndex = 11;
+            this.butLogin.Text = "Đăng nhập";
+            this.butLogin.Click += new System.EventHandler(this.butLogin_Click);
             // 
             // txtUserpwd
             // 
             this.txtUserpwd.BackColor = System.Drawing.Color.Transparent;
-            this.txtUserpwd.BorderColor = System.Drawing.Color.Black;
+            this.txtUserpwd.BorderColor = System.Drawing.Color.DarkBlue;
             this.txtUserpwd.BorderRadius = 15;
             this.txtUserpwd.BorderThickness = 2;
             this.txtUserpwd.Cursor = System.Windows.Forms.Cursors.IBeam;
@@ -161,7 +147,7 @@
             this.txtUserpwd.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.txtUserpwd.ForeColor = System.Drawing.Color.MediumPurple;
             this.txtUserpwd.HoverState.BorderColor = System.Drawing.Color.Silver;
-            this.txtUserpwd.Location = new System.Drawing.Point(57, 254);
+            this.txtUserpwd.Location = new System.Drawing.Point(70, 245);
             this.txtUserpwd.Name = "txtUserpwd";
             this.txtUserpwd.PasswordChar = '●';
             this.txtUserpwd.PlaceholderForeColor = System.Drawing.Color.LightSteelBlue;
@@ -172,25 +158,57 @@
             this.txtUserpwd.UseSystemPasswordChar = true;
             this.txtUserpwd.TextChanged += new System.EventHandler(this.txtUserpwd_TextChanged);
             // 
-            // butLogin
+            // txtUsername
             // 
-            this.butLogin.BackColor = System.Drawing.Color.Transparent;
-            this.butLogin.BorderColor = System.Drawing.Color.BlueViolet;
-            this.butLogin.BorderRadius = 30;
-            this.butLogin.BorderThickness = 3;
-            this.butLogin.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
-            this.butLogin.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
-            this.butLogin.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
-            this.butLogin.DisabledState.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
-            this.butLogin.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
-            this.butLogin.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.butLogin.ForeColor = System.Drawing.SystemColors.Window;
-            this.butLogin.Location = new System.Drawing.Point(79, 380);
-            this.butLogin.Name = "butLogin";
-            this.butLogin.Size = new System.Drawing.Size(358, 65);
-            this.butLogin.TabIndex = 11;
-            this.butLogin.Text = "siticoneGradientButton1";
-            this.butLogin.Click += new System.EventHandler(this.butLogin_Click);
+            this.txtUsername.BackColor = System.Drawing.Color.Transparent;
+            this.txtUsername.BorderColor = System.Drawing.Color.Navy;
+            this.txtUsername.BorderRadius = 15;
+            this.txtUsername.BorderThickness = 2;
+            this.txtUsername.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtUsername.DefaultText = "";
+            this.txtUsername.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtUsername.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtUsername.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtUsername.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtUsername.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(196)))), ((int)(((byte)(255)))));
+            this.txtUsername.FocusedState.ForeColor = System.Drawing.Color.SlateBlue;
+            this.txtUsername.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtUsername.ForeColor = System.Drawing.Color.MediumPurple;
+            this.txtUsername.HoverState.BorderColor = System.Drawing.Color.Silver;
+            this.txtUsername.Location = new System.Drawing.Point(70, 159);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.PasswordChar = '\0';
+            this.txtUsername.PlaceholderForeColor = System.Drawing.Color.LightSteelBlue;
+            this.txtUsername.PlaceholderText = "username";
+            this.txtUsername.SelectedText = "";
+            this.txtUsername.Size = new System.Drawing.Size(361, 50);
+            this.txtUsername.TabIndex = 0;
+            this.txtUsername.TextChanged += new System.EventHandler(this.txtUsername_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(127, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(247, 59);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Đăng Nhập";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // siticonePictureBox1
+            // 
+            this.siticonePictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.siticonePictureBox1.Image = global::GUI.Properties.Resources.shape_transformed;
+            this.siticonePictureBox1.ImageRotate = 0F;
+            this.siticonePictureBox1.Location = new System.Drawing.Point(464, 60);
+            this.siticonePictureBox1.Name = "siticonePictureBox1";
+            this.siticonePictureBox1.Size = new System.Drawing.Size(669, 666);
+            this.siticonePictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.siticonePictureBox1.TabIndex = 11;
+            this.siticonePictureBox1.TabStop = false;
             // 
             // customPictureBox1
             // 
@@ -198,31 +216,50 @@
             this.customPictureBox1.BorderColor = System.Drawing.Color.RoyalBlue;
             this.customPictureBox1.BorderColor2 = System.Drawing.Color.HotPink;
             this.customPictureBox1.BorderLineStyle = System.Drawing.Drawing2D.DashStyle.Solid;
-            this.customPictureBox1.BorderSize = 4;
+            this.customPictureBox1.BorderSize = 2;
             this.customPictureBox1.GradientAngle = 50F;
-            this.customPictureBox1.Location = new System.Drawing.Point(-219, -77);
+            this.customPictureBox1.Image = global::GUI.Properties.Resources.Book;
+            this.customPictureBox1.Location = new System.Drawing.Point(-410, -78);
             this.customPictureBox1.Name = "customPictureBox1";
-            this.customPictureBox1.Size = new System.Drawing.Size(870, 870);
+            this.customPictureBox1.Size = new System.Drawing.Size(906, 906);
             this.customPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.customPictureBox1.TabIndex = 7;
             this.customPictureBox1.TabStop = false;
+            // 
+            // customPanel1
+            // 
+            this.customPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.customPanel1.BorderRadius = 170;
+            this.customPanel1.Controls.Add(this.siticoneCustomGradientPanel1);
+            this.customPanel1.ForeColor = System.Drawing.Color.Black;
+            this.customPanel1.GradientAngle = 90F;
+            this.customPanel1.GradientBottomColor = System.Drawing.Color.CadetBlue;
+            this.customPanel1.GradientTopColor = System.Drawing.Color.DodgerBlue;
+            this.customPanel1.Location = new System.Drawing.Point(570, 149);
+            this.customPanel1.Name = "customPanel1";
+            this.customPanel1.Size = new System.Drawing.Size(473, 431);
+            this.customPanel1.TabIndex = 12;
             // 
             // formLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1235, 715);
-            this.Controls.Add(this.siticoneCustomGradientPanel1);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1099, 715);
+            this.Controls.Add(this.customPanel1);
             this.Controls.Add(this.customPictureBox1);
             this.Controls.Add(this.exit);
             this.Controls.Add(this.siticoneControlBox2);
+            this.Controls.Add(this.siticonePictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "formLogin";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "fLogin";
             this.siticoneCustomGradientPanel1.ResumeLayout(false);
             this.siticoneCustomGradientPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.siticonePictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.customPictureBox1)).EndInit();
+            this.customPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -239,5 +276,7 @@
         private Siticone.Desktop.UI.WinForms.SiticoneTextBox txtUserpwd;
         private Siticone.Desktop.UI.WinForms.SiticoneTextBox txtUsername;
         private System.Windows.Forms.Label label1;
+        private Siticone.Desktop.UI.WinForms.SiticonePictureBox siticonePictureBox1;
+        private CustomPanel customPanel1;
     }
 }

cc0848af05183fcb1ffdbd6fc4dec5ab220a2e24
eugeous <121101254+Eugeou@users.noreply.github.com>
2023-06-07 15:39:34 +0700

    Thêm Custome Panel (Custom Element tự thiết kế) Custom panel là panel đặc biệt có 4 góc bo tròn có thể thay đổi độ tròn nhờ thuộc tính Border Radius được thêm vào Được viết thêm thuoccj tính Gradient color , có thể thêm 2 màu cùng lúc vào panel Là panel tự viết thêm, không có hỗ trợ sẵn

---
+++ GUI/CustomPanel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public class CustomPanel : Panel
+    {
+        //fields
+        private int borderRadius = 30;
+        private float gradientAngle = 90F;
+        private Color gradientTopColor = Color.DodgerBlue;
+        private Color gradientBottomColor = Color.CadetBlue;
+        Timer timer = new Timer();
+        //Constructor
+        public CustomPanel()
+        {
+            this.BackColor = Color.White;
+            this.ForeColor = Color.Black;
+            this.Size = new Size(350, 300);
+            DoubleBuffered = true;
+        }
+
+    }        
+}

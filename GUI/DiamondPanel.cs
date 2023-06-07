fa543261da282a744f65f72eff8ea8f855307c1f
eugeous <121101254+Eugeou@users.noreply.github.com>
2023-06-07 17:29:42 +0700

    Tạo Diamond Panel (Panel hình thoi) Là 1 panel có hình thoi với 4 góc có thể bo tròn (4 góc của cái hình thoi trong panel chứ không phải panel) hoặc tạo hình đặc biệt Có thể thêm 2 màu cùng lúc tạo hiệu ứng gradient Các properties GradientTop và BottomColor 2 thuộc tính màu trên và dưới của panel GradientAngle góc tương quan giữa 2 màu BorderRadius góc bo của hình thoi (ko phải của panel)

--- GUI/DiamondPanel.cs
+++ GUI/DiamondPanel.cs
@@ -1,1 +1,31 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+using System.ComponentModel;
+
+namespace GUI
+{
+    public class DiamondPanel : Panel
+    {
+        // Properties
+        public Color GradientTopColor { get; set; }
+        public Color GradientBottomColor { get; set; }
+        public int GradientAngle { get; set; }
+        public float BorderRadius { get; set; }
+
+        // Constructor
+        public DiamondPanel()
+        {
+            GradientTopColor = Color.White;
+            GradientBottomColor = Color.Gray;
+            GradientAngle = 45;
+            BorderRadius = 10;
+        }
+
+    }  
+}

eaa34eb4b0d060825199026bead1d0be3e5718c0
SONLE03 <sonle102003@gmail.com>
2023-06-08 01:20:04 +0700

    thêm dal chức năng

---
+++ DAL/DALChucNang.cs
@@ -0,0 +1,40 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DALChucNang
+    {
+        private static DALChucNang instance;
+
+        public static DALChucNang Instance
+        {
+            get
+            {
+                if (instance == null) instance = new DALChucNang();
+                return instance;
+            }
+            set => instance = value;
+        }
+
+        public List<CHUCNANG> GetAllChucNang()
+        {
+            return QLTVEntities.Instance.CHUCNANGs.AsNoTracking().ToList();
+        }
+
+        public CHUCNANG GetChucNangById(int id)
+        {
+            return QLTVEntities.Instance.CHUCNANGs.Find(id);
+        }
+
+        public CHUCNANG GetChucNangByMa(string ma)
+        {
+            var res = QLTVEntities.Instance.CHUCNANGs.Where(c => c.MaChucNang == ma);
+            return (res.Any() ? res.First() : null);
+        }
+    }
+}

f2d793cb8aa43bbf4c13102586a4b7288dd68c87
SONLE03 <sonle102003@gmail.com>
2023-06-12 19:29:47 +0700

    thêm dal chi tiết báo cáo lượt mượn theo thể loại

---
+++ DAL/DALCT_BCLuotMuonTheoTheLoai.cs
@@ -0,0 +1,92 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DALCT_BCLuotMuonTheoTheLoai
+    {
+        private static DALCT_BCLuotMuonTheoTheLoai instance;
+
+        public static DALCT_BCLuotMuonTheoTheLoai Instance
+        {
+            get
+            {
+                if (instance == null) instance = new DALCT_BCLuotMuonTheoTheLoai();
+                return instance;
+            }
+            set => instance = value;
+        }
+
+        public List<CT_BCLUOTMUONTHEOTHELOAI> GetAllCTBC()
+        {
+            return QLTVEntities.Instance.CT_BCLUOTMUONTHEOTHELOAI.AsNoTracking().ToList();
+        }
+
+        public CT_BCLUOTMUONTHEOTHELOAI GetCTBC(int idBC, int idTheLoai)
+        {
+            return QLTVEntities.Instance.CT_BCLUOTMUONTHEOTHELOAI.Find(new object[] { idBC, idTheLoai });
+        }
+
+        public bool AddCTBC(int idBC, int idTheLoai, int soLuotMuon)
+        {
+            try
+            {
+                var ct = new CT_BCLUOTMUONTHEOTHELOAI
+                {
+                    idBaoCao = idBC,
+                    BCLUOTMUONTHEOTHELOAI = DALBCLuotMuonTheoTheLoai.Instance.GetBaoCaoById(idBC),
+                    idTheLoai = idTheLoai,
+                    THELOAI = DALTheLoai.Instance.GetTheLoaiById(idTheLoai),
+                    SoLuotMuon = soLuotMuon
+                };
+                QLTVEntities.Instance.CT_BCLUOTMUONTHEOTHELOAI.Add(ct);
+                QLTVEntities.Instance.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException.ToString());
+                return false;
+            }
+        }
+
+        public bool UpdCTBC(int idBC, int idTL, int soLuotMuon)
+        {
+            try
+            {
+                var ct = GetCTBC(idBC, idTL);
+                if (ct == null) return false;
+                ct.SoLuotMuon = soLuotMuon;
+                QLTVEntities.Instance.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException.ToString());
+                return false;
+            }
+        }
+
+        public bool DelCTBC(int idBC, int idTL)
+        {
+            try
+            {
+                var ct = GetCTBC(idBC, idTL);
+                if (ct == null) return false;
+
+                QLTVEntities.Instance.CT_BCLUOTMUONTHEOTHELOAI.Remove(ct);
+                QLTVEntities.Instance.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException.ToString());
+                return false;
+            }
+        }
+    }
+}

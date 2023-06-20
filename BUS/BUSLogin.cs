f6b4d46a73501d267935593764d96497a528106c
Thông Phạm Duy <21522645@ms.uit.edu.vn>
2023-06-20 17:41:44 +0700

    Hoàn thành file BUSLogin

---
+++ BUS/BUSLogin.cs
@@ -0,0 +1,36 @@
+﻿using DAL;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class BUSLogin
+    {
+        private static BUSLogin instance;
+        public static BUSLogin Instance
+        {
+            get
+            {
+                if (instance == null) instance = new BUSLogin();
+                return instance;
+            }
+            set => instance = value;
+        }
+
+        public int checkValidLogin(string usrname, string usrpwd)
+        { 
+            foreach (var user in DALNguoiDung.Instance.GetAllNguoiDung())
+            {
+                if (user.TenDangNhap.Equals(usrname) && user.MatKhau.Equals(usrpwd))
+                {
+                    return user.id;
+                }
+            }
+            return -1;
+        }
+    }
+}

6dd827d63a2e783ff6b0680b1a6b76c5b526a9e1
eugeous <121101254+Eugeou@users.noreply.github.com>
2023-06-22 22:36:17 +0700

    Hoàn thiện UI cho form fChangePassword

--- GUI/Properties/Resources.Designer.cs
+++ GUI/Properties/Resources.Designer.cs
@@ -253,6 +253,16 @@ namespace GUI.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap password {
+            get {
+                object obj = ResourceManager.GetObject("password", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap phone_book {
             get {
                 object obj = ResourceManager.GetObject("phone-book", resourceCulture);

c630e2bee6373c9c0123cb7f759fc5a56683e1d8
eugeous <121101254+Eugeou@users.noreply.github.com>
2023-06-22 21:18:24 +0700

    Hoàn thiện UI cho form fEditTheLoai

--- GUI/Properties/Resources.Designer.cs
+++ GUI/Properties/Resources.Designer.cs
@@ -283,6 +283,16 @@ namespace GUI.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap sketch {
+            get {
+                object obj = ResourceManager.GetObject("sketch", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap tag {
             get {
                 object obj = ResourceManager.GetObject("tag", resourceCulture);

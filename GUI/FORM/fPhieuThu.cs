19a405d84f6d55b3a2b2d390c098da54518058f5
Takae <93442902+takaesa@users.noreply.github.com>
2023-07-02 14:00:24 +0700

    binding data cho fPhieuThu

--- GUI/FORM/fPhieuThu.cs
+++ GUI/FORM/fPhieuThu.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DTO;
+using BUS;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,23 @@ namespace GUI.FORM
 {
     public partial class fPhieuThu : Form
     {
+        private DOCGIA docgia;
         public fPhieuThu()
         {
             InitializeComponent();
+            init();
+        }
+        private int TienThu;
+        private void init()
+        {
+            TienThu = 0;
+            dateNgayLap.Value = DateTime.Now.Date;
+            comboDocGia.DataSource = BUSDocGia.Instance.GetAllDocGia();
+            comboDocGia.ValueMember = "ID";
+            comboDocGia.DisplayMember = "MaDocGia";
+            comboDocGia.SelectedIndex = 0;
+            docgia = BUSDocGia.Instance.GetDocGiaById(Convert.ToInt32(comboDocGia.SelectedValue));
+            labelNoHienTai.Text = docgia.TongNoHienTai.ToString();
         }
     }
 }

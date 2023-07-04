fd201fc3ab966d651e71b769e63e5448f6118eeb
Thông Phạm Duy <21522645@gm.uit.edu.vn>
2023-07-04 15:23:51 +0700

    Hiển thị sách trong database ra datagridview

--- GUI/UserControls/ucHome_TCS.cs
+++ GUI/UserControls/ucHome_TCS.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BUS;
+using DTO;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,28 @@ namespace GUI.UserControls
         public ucHome_TCS()
         {
             InitializeComponent();
+            Binding(BUSSach.Instance.GetAllSach());
+            comboTheLoai.DataSource = BUSTheLoai.Instance.GetAllTheLoai();
+            comboTheLoai.ValueMember = "id";
+            comboTheLoai.DisplayMember = "TenTheLoai";
+            List<string> TinhTrangList = new List<string> { "Còn", "Hết" };
+            comboTinhTrang.DataSource = TinhTrangList;
+        }
+
+        private void Binding(List<SACH> sACHes)
+        {
+            SachGrid.Rows.Clear();
+            foreach (SACH sach in SachList)
+                if (sach.DaAn == 0)
+                {
+                    string tacgia = "";
+                    foreach (TACGIA tg in sach.TUASACH.TACGIAs)
+                        tacgia += tg.TenTacGia + ", ";
+                    if (tacgia != "")
+                        tacgia = tacgia.Remove(tacgia.Length - 2, 2);
+                    string Con = (sach.SoLuongConLai > 0) ? "Còn" : "Hết";
+                    SachGrid.Rows.Add(sach.MaSach, sach.TUASACH.TenTuaSach, sach.TUASACH.THELOAI.TenTheLoai, sach.NamXB, sach.NhaXB, tacgia, Con);
+                }
         }
     }
 }

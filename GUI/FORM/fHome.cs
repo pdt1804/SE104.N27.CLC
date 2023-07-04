550226f6fb8a730f0c0f445e102fe48f1dfd4fec
Thông Phạm Duy <21522645@gm.uit.edu.vn>
2023-07-04 14:34:41 +0700

    Hoàn thành hiển thị thông tin tài khoản độc giả

--- GUI/FORM/fHome.cs
+++ GUI/FORM/fHome.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BUS;
+using DTO;
+using GUI.UserControls;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,16 +16,29 @@ namespace GUI
     public partial class fHome : Form
     {
 
+        private NGUOIDUNG user;
         public fHome(int id)
         {
             InitializeComponent();
 
-            
+            user = BUSNguoiDung.Instance.GetNguoiDungById(id);
+            container.Controls.Add(new ucHome_TTTK(id) { Dock = DockStyle.Fill, Name = "tttk" });
+            container.Controls.Add(new ucHome_TCS() { Dock = DockStyle.Fill, Name = "tcsach" });
+            butTTTK.Checked = true;
+            Control[] con = container.Controls.Find("tttk", false);
+            con[0].BringToFront();
         }
 
         private void butTTTK_Click(object sender, EventArgs e)
         {
-            
+            Control[] con = container.Controls.Find("tttk", false);
+            con[0].BringToFront();
+        }
+
+        private void butTraCuu_Click(object sender, EventArgs e)
+        {
+            Control[] con = container.Controls.Find("tcsach", false);
+            con[0].BringToFront();
         }
 
         private void butManager_Click(object sender, EventArgs e)
@@ -30,9 +46,6 @@ namespace GUI
             
         }
 
-        private void butTraCuu_Click(object sender, EventArgs e)
-        {
-        }
 
     }
 }
--- GUI/UserControls/ucHome_TTTK.cs
+++ GUI/UserControls/ucHome_TTTK.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BUS;
+using DTO;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,36 @@ namespace GUI.UserControls
 {
     public partial class ucHome_TTTK : UserControl
     {
-        public ucHome_TTTK()
+        public NGUOIDUNG user { get; set; }
+        public DOCGIA docgia { get; set; }
+
+        public ucHome_TTTK(int id)
         {
             InitializeComponent();
+            user = new NGUOIDUNG();
+            user = BUSNguoiDung.Instance.GetNguoiDungById(id);
+            docgia = new DOCGIA();
+            foreach (var p in BUSDocGia.Instance.GetAllDocGia())
+            {
+                if (p.idNguoiDung == id)
+                {
+                    docgia = p;
+                    break;
+                }    
+            }    
+            if (docgia != null)
+            {
+                labelDiaChi.Text = docgia.DiaChi;
+                labelEmail.Text = user.EMAIL;
+                labelHoTen.Text = docgia.TenDocGia;
+                labelLoaiDG.Text = docgia.LOAIDOCGIA.TenLoaiDocGia;
+                labelMaDG.Text = docgia.MaDocGia;
+                labelNgayHetHan.Text = docgia.NgayHetHan.ToString();
+                labelNgayLapThe.Text = docgia.NgayLapThe.ToString();
+                labelNgaySinh.Text = docgia.NgaySinh.ToString();
+                labelTenDangNhap.Text = user.TenDangNhap;
+                labelTongNo.Text = docgia.TongNoHienTai.ToString();
+            }    
         }
     }
 }

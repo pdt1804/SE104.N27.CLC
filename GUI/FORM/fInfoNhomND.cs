65f1cefcb8f9a5610cdc7a65f47e8e4967dddfe4
Takae <93442902+takaesa@users.noreply.github.com>
2023-07-05 20:50:43 +0700

    hoàn thành bind info nhóm người dùng

--- GUI/FORM/fInfoNhomND.cs
+++ GUI/FORM/fInfoNhomND.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BUS;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,19 @@ namespace GUI.FORM
 {
     public partial class fInfoNhomND : Form
     {
-        public fInfoNhomND()
+        private static int id;
+        public fInfoNhomND(int _id)
         {
             InitializeComponent();
+            id = _id;
+            Bind();
+        }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+        private void Bind()
+        {
+            var nnd = BUSNhomNguoiDung.Instance.GetNhomNguoiDungById(id);
+            this.dsChucNang.DataSource = nnd.CHUCNANGs.ToList();
+            this.labelMaNhom.Text = "Mã Nhóm Người Dùng: " + nnd.MaNhomNguoiDung.ToString();
+            this.labelTenNhom.Text = "Tên Nhóm Người Dùng: " + nnd.TenNhomNguoiDung;
         }
     }
 }

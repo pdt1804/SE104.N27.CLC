37a232fca6b1e7c7cba457bc6f88a4583f7b3ef8
Takae <93442902+takaesa@users.noreply.github.com>
2023-07-05 16:15:05 +0700

    bind data trước khi thêm nhóm người dùng

--- GUI/FORM/fAddNhomND.cs
+++ GUI/FORM/fAddNhomND.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BUS;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,15 @@ namespace GUI.FORM
         public fAddNhomND()
         {
             InitializeComponent();
+            Bind();
+        }
+        private void Bind()
+        {
+            var dscn = BUSChucNang.Instance.GetAllChucNang();
+            foreach(var cn in dscn)
+            {
+                dsChucNang.Rows.Add("0", cn.MaChucNang, cn.TenChucNang, cn.TenManHinh);
+            }
         }
     }
 }
